3d8590362d170cbe7c7ba208c6879ac6adaf01bb
Yusmin Fermin <yusmifermin@gmail.com>
2020-05-12 17:48:54 +0200

    Backup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { faDesktop, faReply, faFlask, faHome, faPhone } from '@fortawesome/free-solid-svg-icons'
+import {
+  faFacebook,
+  faTwitter,
+  faGooglePlus,
+  faLinkedin,
+  faPinterest
+} from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'yuyu';
+  title = 'yuyu'
+  faDesktop = faDesktop
+  faReply = faReply
+  faFlask = faFlask
+  faHome = faHome
+  faPhone = faPhone
+  faFacebook = faFacebook
+  faTwitter = faTwitter
+  faGooglePlus = faGooglePlus
+  faLinkedin = faLinkedin
+  faPinterest = faPinterest
 }
